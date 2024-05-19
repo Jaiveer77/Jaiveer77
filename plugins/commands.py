3bc4e4cec026c800da9df020167b146587b8b9e8
Jaiveer77 <jaibb959@gmail.com>
2024-05-19 12:20:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -593,7 +593,8 @@ async def start(client, message):
             logger.exception(e)
             f_caption=f_caption
     if f_caption is None:
-        f_caption = f"@VJ_Bots  {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))}"
+        f_caption = f"FREE MOVIES CHANNEL  @moviesaddaorg 
+        REQUEST ANY MOVIE @requestanymoviefilm   {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))}"
     if not await db.has_premium_access(message.from_user.id):
         if not await check_verification(client, message.from_user.id) and VERIFY == True:
             btn = [[
