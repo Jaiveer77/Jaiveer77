2fa336f51b4a10c4934018f0fbe44c618f0aa165
Jaiveer77 <jaibb959@gmail.com>
2024-07-14 10:11:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -92,8 +92,8 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Shortlink Info
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'L0hHiMYxs9eq5f0IFdFes2Q9zZk2')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'easysky.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', '10be208ccc22c335682214bb939e78394ea366ff')
 
 # Others
 MAX_B_TN = environ.get("MAX_B_TN", "5")
